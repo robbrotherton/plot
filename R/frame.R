b1d3255b77fdbaaf8f9c5453e710121560652aa8
robbrotherton <robert.brotherton@gmail.com>
2022-06-03 15:06:40 -0400

    Initial commit

---
+++ R/frame.R
@@ -0,0 +1,133 @@
+frame <- function(df,
+                  paper_size = "A4",
+                  frame_size = "11x14",
+                  frame_depth = .5,
+                  matt_size = NULL,
+                  matt_color = "snow1",
+                  frame_color = "#B39671",
+                  show_paper_edges = FALSE) {
+
+  col_names <- colnames(df)
+
+  # Check for group var; if missing, add one with warning
+  has_group_var <- "group" %in% col_names
+
+  if (!has_group_var) {
+    df$group <- 1
+    message("No group variable present.")
+  }
+
+  # Check for color var
+  has_color_var <- "color" %in% col_names
+
+  # Make base plot, with or without colors
+
+  if (has_color_var) {
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_path(data = df, ggplot2::aes(x, y, group = group, color = color)) +
+      ggplot2::scale_color_identity()
+  } else {
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_path(data = df, ggplot2::aes(x, y, group = group))
+  }
+
+  p + geom_frame(paper_size,
+                 frame_size,
+                 frame_depth,
+                 matt_size,
+                 matt_color,
+                 frame_color,
+                 show_paper_edges)
+
+}
+
+
+make_frame <- function(x, y, cx, cy, w, fill, line = NA) {
+  x <- x/2
+  y <- y/2
+  xCoords <- c(cx-x-w,cx+x+w,cx+x+w,cx-x,cx-x,cx-x-w,cx-x-w)
+  yCoords <- c(cy+y+w,cy+y+w,cy+y,cy+y,cy-y-w,cy-y-w,cy+y+w)
+
+  ggplot2::geom_polygon(inherit.aes = FALSE,
+                        data = NULL,
+                        ggplot2::aes(
+                          x = c(xCoords, -xCoords+cx*2),
+                          y = c(yCoords, -yCoords+cy*2),
+                          group = rep(1:2, each = 7)
+                        ), fill = fill, color = line)
+}
+
+geom_frame <- function(paper_size = "A4",
+                       frame_size = "11x14",
+                       frame_depth = .5,
+                       matt_size = NULL,
+                       matt_color = "snow1",
+                       frame_color = "#B39671",
+                       show_paper_edges = FALSE) {
+
+  out <- list()
+
+  if(stringr::str_detect(paper_size, "x"))  {
+    paper_width <- strsplit(paper_size, "[x]")[[1]][1] |> as.numeric()*25.4
+    paper_height <- strsplit(paper_size, "[x]")[[1]][2] |> as.numeric()*25.4
+  }
+  if(paper_size=="A4") {
+    paper_width <- 8.25*25.4; paper_height <- 11.75*25.4
+  }
+  if(paper_size=="A3") {
+    paper_width <- 11.75*25.4; paper_height <- 16.5*25.4
+  }
+
+  frame_width <- strsplit(frame_size, "[x]")[[1]][1] |> as.numeric()*25.4
+  frame_height <- strsplit(frame_size, "[x]")[[1]][2]|> as.numeric()*25.4
+
+  if(!is.null(matt_size)) {
+    matt_width <- strsplit(matt_size, "[x]")[[1]][1]  |> as.numeric()*25.4
+    matt_height <- strsplit(matt_size, "[x]")[[1]][2] |> as.numeric()*25.4
+    matt_depth <- (frame_width - matt_width)/2
+  }
+
+  frame_depth <- frame_depth*25.4
+
+  xcenter <- paper_width/2
+  ycenter <- paper_height/2
+
+  if(!is.null(matt_size)) {
+    out[[length(out)+1]] <- make_frame(matt_width,
+                                       matt_height,
+                                       xcenter,
+                                       ycenter,
+                                       w = matt_depth,
+                                       fill = matt_color)
+
+    out[[length(out)+1]] <- make_frame(matt_width,
+                                       matt_height,
+                                       xcenter,
+                                       ycenter,
+                                       w = 0,
+                                       fill = NA,
+                                       line = 'grey')
+  }
+
+  out[[length(out)+1]] <- make_frame(frame_width,
+                                     frame_height,
+                                     xcenter,
+                                     ycenter,
+                                     frame_depth,
+                                     fill = frame_color)
+
+  if(show_paper_edges) {
+
+    out[[length(out)+1]] <- make_frame(paper_width,
+                                       paper_height,
+                                       xcenter,
+                                       ycenter,
+                                       w = 0,
+                                       fill = NA,
+                                       line = 'red')
+  }
+
+  list(out, ggplot2::coord_fixed(), ggplot2::theme_void())
+
+}
+
---
+++ R/gcode.R
@@ -0,0 +1,191 @@
+write_gcode <- function(data, filename, pic = NULL) {
+
+  time <- plotting_time(data)
+  # include a check here for out-of-bounds X and Y values
+  if(min(data$x) < 0 | min(data$y) < 0 | max(data$x) > 215 | max(data$y) > 300) warning("out of bounds")
+
+
+  start <- "M280P0S30\nG90 G21"
+  pen_up <- "G4 P50\nM280P0S30\nG1 F3000"
+  pen_down <- "G4 P50\nM280P0S0\nG1 F800"
+  finish <- "G1 X0 Y0"
+
+  if(!is.null(data$color)) {
+
+    filestem <- gsub(".gcode", "", filename)
+    colors <- unique(data$color)
+    print(colors)
+    n <- length(colors)
+    files <- paste0(filestem, "_", colors, ".gcode")
+  } else {
+    files <- filename
+    data$color <- 1
+    colors <- 1
+    n <- 1
+  }
+
+  for(j in 1:n) {
+
+    df_list <- data |>
+      dplyr::filter(color == colors[j]) |>
+      dplyr::group_by(group) |>
+      dplyr::group_split()
+
+    r2 <- function(x) sprintf("%.2f", x)
+
+    p <- purrr::map(seq_along(df_list), function(i) {
+      c(paste0("G1 X", r2(df_list[[i]]$x[1]), " Y", r2(df_list[[i]]$y[1])),
+        # need to duplicate coords to move while pen up between groups
+        pen_down,
+        paste0("G1 X", r2(df_list[[i]]$x), " Y", r2(df_list[[i]]$y)),
+        pen_up)
+    })
+
+    # run this to write the .gcode text file
+    write.table(start, files[j], append= F, row.names=F, col.names=F, quote=F)
+
+    lapply(p, function(x) {
+      write.table( data.frame(x), files[j], append= T, row.names=F, col.names=F, quote=F)
+    })
+
+    write.table(finish, files[j], append= T, row.names=F, col.names=F, quote=F)
+
+    message(paste(files[j], "created"))
+
+    message(paste("approx. plotting time:", time))
+
+  }
+
+  if(!is.null(pic)) {
+    ggplot2::ggsave(plot = pic,
+           filename = paste0(filename, ".png"),
+           width = 8.25,
+           height = 11.75)
+  }
+
+}
+
+
+read_gcode <- function(file, animate = FALSE) {
+
+  data <- read.delim(file, col.names = "text") |>
+    dplyr::filter(stringr::str_detect(text, "G1 X") |
+                  stringr::str_detect(text, "G1 F800") |
+                  stringr::str_detect(text, "G1 F3000")) |>
+    dplyr::mutate(pen_up = stringr::str_detect(lag(text, default = "G1 F3000"), "G1 F3000")) |>
+    dplyr::filter(!pen_up, !stringr::str_detect(text, "G1 F3000")) |>
+    dplyr::mutate(group = cumsum(ifelse(stringr::str_detect(text, "G1 F"), 1, 0))) |>
+    dplyr::filter(stringr::str_detect(text, "G1 X")) |>
+    tidyr::separate(text, sep = " Y", into = c("x", "y")) |>
+    dplyr::mutate(x = gsub(pattern = "G1 X", replacement = "", x = x)) |>
+    dplyr::mutate_all(as.numeric) |>
+    dplyr::select(-pen_up)
+
+  # pic <- ggplot(data, aes(x, y, group = group)) +
+  #   geom_path() +
+  #   coord_fixed()
+  #
+  # print(pic)
+
+  return(data)
+
+}
+
+
+plotting_time <- function(df, plotter_speed = 800) {
+
+  # group by colors
+  df <- dplyr::mutate(df, dist = sqrt((x-dplyr::lag(x, default = 0))^2+(y-dplyr::lag(y, default = 0))^2))
+
+  total_distance <- sum(df$dist)
+
+  approx_mins <- total_distance / plotter_speed
+
+  chron::times(approx_mins/24/60)
+
+}
+
+
+# plotting_time <- function(gcode) {
+#
+#   # read in a gcode file, including pen up movements
+#   data <- read.delim(gcode, col.names = "text") |>
+#     dplyr::filter(stringr::str_detect(text, "G1 X")) |>
+#     tidyr::separate(text, sep = " Y", into = c("x", "y")) |>
+#     dplyr::mutate(x = gsub(pattern = "G1 X", replacement = "", x = x)) |>
+#     dplyr::mutate_all(as.numeric) |>
+#     dplyr::mutate(dist = sqrt((x-dplyr::lag(x, default = 0))^2+(y-dplyr::lag(y, default = 0))^2))
+#
+#   total_distance <- sum(data$dist)
+#
+#   approx_mins <- total_distance / 800
+#
+#   chron::times(approx_mins/24/60)
+#
+# }
+
+
+
+resize_for_plotter <- function(df,
+                               paper_width = 210, paper_height = 297,
+                               # max_width = paper_width*.9,
+                               # max_height = paper_height*.9,
+                               max_dim = NULL) {
+
+  x_range <- max(df$x) - min(df$x)
+  y_range <- max(df$y) - min(df$y)
+
+  if(!is.null(max_dim)) {
+    if(x_range >= y_range) {
+      ratio <- max_dim/x_range
+    } else {
+      ratio <- max_dim/y_range
+    }
+  } else {
+    if(x_range <= y_range) {
+      ratio <- paper_width/x_range
+    } else {
+      ratio <- paper_height/y_range
+    }
+  }
+
+  # scale to max dim
+  df <- df |>
+    mutate(x = scale(x, center = 0, scale = 1/ratio),
+           y = scale(y, center = 0, scale = 1/ratio))
+
+  # now nudge so min values are 0,0
+  nudge_x <- min(df$x)
+  nudge_y <- min(df$y)
+
+  df <- df |>
+    mutate(x = x - nudge_x,
+           y = y - nudge_y)
+  # df <- df |>
+  #   mutate(x = x * ratio,
+  #          # x = x - min(x),
+  #          # x = x + (paper_width-max(x))*.5,
+  #          y = y * ratio,
+  #          # y = y - min(y),
+  #          # y = y + (paper_height-max(y))*.5
+  #   )
+  #
+  # now position in center of page
+  image_center_x <- max(df$x)/2
+  image_center_y <- max(df$y)/2
+  page_center_x <- paper_width/2
+  page_center_y <- paper_height/2
+  # print(y_center)
+  df <- df |>
+    mutate(x = x - (image_center_x - page_center_x),
+           y = y - (image_center_y - page_center_y)
+    )
+
+  # include a check here for out-of-bounds X and Y values
+  if(min(df$x) < 0 | min(df$y) < 0 | max(df$x) > 215 | max(df$y) > 300) {
+    warning("out of bounds")
+  }
+
+  return(df)
+
+}
